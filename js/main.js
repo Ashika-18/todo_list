3ed9f5993797dc51dc24cb7fda830b24ca2b920c
Ashika <117647146+Ashika-18@users.noreply.github.com>
2023-02-26 12:20:58 +0900

    fin

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-const taskValue = document.getElementByClassName('task_value')[0];
-const taskSubmit = document.getElementByClassName('task_submit')[0];
-const taskList = document.getElementByClassName('task_list')[0];
+const taskValue = document.getElementsByClassName('task_value')[0];
+const taskSubmit = document.getElementsByClassName('task_submit')[0];
+const taskList = document.getElementsByClassName('task_list')[0];
 
 const addTasks = (task) => {
     // 入力したタスクを追加　表示
-    const listItem = document.crateElement('li');
+    const listItem = document.createElement('li');
     const showItem = taskList.appendChild(listItem);
-    showItem.innerHtml = task;
+    showItem.innerHTML = task;
 
     //タスクに削除ボタンを付与
     const deleteButton = document.createElement('button');
@@ -18,4 +18,19 @@ const addTasks = (task) => {
         evt.preventDefault();
         deleteTasks(deleteButton);
     });
-}
+};
+
+//削除ボタンにタスクを消す機能を付与
+const deleteTasks = (deleteButton) => {
+    const chosenTask = deleteButton.closest('li');
+    taskList.removeChild(chosenTask);
+};
+
+//追加ボタンをクリックし、イベントを発動 (タスク追加)
+
+taskSubmit.addEventListener('click', evt => {
+    evt.preventDefault();
+    const task = taskValue.value;
+    addTasks(task);
+    taskValue.value = '';
+});
